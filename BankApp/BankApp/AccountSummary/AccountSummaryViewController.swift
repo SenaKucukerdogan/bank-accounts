dda426b5cd92e7d6834e0e328dd51443cb92b865
[SenaKucukerdogan] <sena.kucukerdogan@gmail.com>
2024-12-29 20:13:53 +0300

    fead: Add view model

--- BankApp/BankApp/AccountSummary/AccountSummaryViewController.swift
+++ BankApp/BankApp/AccountSummary/AccountSummaryViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class AccountSummaryViewController: UIViewController {
-    
-    let games = ["Pacman", "Space Invaders", "Space Patrol"]
-    
+
+    var accounts: [AccountSummaryCell.ViewModel] = []
+
     var tableView = UITableView()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -23,54 +23,88 @@ extension AccountSummaryViewController {
     private func setup() {
         setupTableView()
         setupTableHeaderView()
+        fetchData()
     }
-    
+
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
-        
-        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+
+        tableView.register(
+            AccountSummaryCell.self,
+            forCellReuseIdentifier: AccountSummaryCell.reuseID)
         tableView.rowHeight = AccountSummaryCell.rowHeight
         tableView.tableFooterView = UIView()
-        
+
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
-        
+
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            tableView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
-    
+
     private func setupTableHeaderView() {
-        let header = AccountSummaryHeaderView(frame: .zero) // giving .zero meaning; no initial size
-        
-        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize) // The height
-        size.width = UIScreen.main.bounds.width // The width
+        let header = AccountSummaryHeaderView(frame: .zero)  // giving .zero meaning; no initial size
+
+        var size = header.systemLayoutSizeFitting(
+            UIView.layoutFittingCompressedSize)  // The height
+        size.width = UIScreen.main.bounds.width  // The width
         header.frame.size = size
-        
+
         tableView.tableHeaderView = header
     }
 }
 
 extension AccountSummaryViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        guard !accounts.isEmpty else { return UITableViewCell() }
+
+        let cell =
+            tableView.dequeueReusableCell(
+                withIdentifier:AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
+        let account = accounts[indexPath.row]
+        cell.configure(with: account)
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return games.count
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
+        return accounts.count
     }
 }
 
 extension AccountSummaryViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
+    func tableView(
+        _ tableView: UITableView, didSelectRowAt indexPath: IndexPath
+    ) {
+
+    }
+}
+
+extension AccountSummaryViewController {
+    private func fetchData() {  // dummy data
+        let savings = AccountSummaryCell.ViewModel(
+            accountType: .Banking, accountName: "Basic Saving")
+        let visa = AccountSummaryCell.ViewModel(
+            accountType: .CreditCard, accountName: "Visa Avion Card")
+        let Investment = AccountSummaryCell.ViewModel(
+            accountType: .Investment, accountName: "Tax-Free Saver")
+
+        accounts.append(savings)
+        accounts.append(visa)
+        accounts.append(Investment)
     }
 }
